4d70a8bad5c6419159299683e0e2d7c578de0b2e
Marcin Wosinek <marcin.wosinek@gmail.com>
2018-01-25 09:08:50 +0100

    refactor the malaria deaths plot

--- deaths-by-region/deaths-by-region.r
+++ deaths-by-region/deaths-by-region.r
@@ -7,33 +7,31 @@ malaria_deaths <- read.csv(paste(dirname(sys.frame(1)$ofile), "/malaria-deaths-b
 
 # WRANGLING
 colnames(malaria_deaths) <- colnames(malaria_deaths) %>% tolower()
-malaria_deaths <- malaria_deaths %>% 
-  rename(zgony = malaria.deaths..ihme..2017...deaths.) %>% 
-  rename(rok = year) %>% 
-  rename(region = entity) %>% 
-  select(region, rok, zgony)
+malaria_deaths <- malaria_deaths %>%
+  rename(deaths = malaria.deaths..ihme..2017...deaths.) %>%
+  rename(region = entity) %>%
+  select(region, year, deaths)
 
 # INSPECTION  R
 glimpse(malaria_deaths)
-summary(malaria_deaths$zgony)
+summary(malaria_deaths$deaths)
 
 # VISUALISATION
 plot <- malaria_deaths %>%
-  filter(zgony > 10000) %>%
-  ggplot() + geom_area(aes(rok, zgony, fill = region),
+  filter(deaths > 6000) %>%
+  ggplot() + geom_area(aes(year, deaths, fill = region),
                        alpha = .9,
                        position = "stack") +
-  facet_wrap( ~ region) +
   labs(
     title = "Żniwo malarii",
     x = "Lata",
     y = "Zgony",
     caption = "Oparto o ourworldindata.org/malaria CC BY-SA"
   ) +
-  theme_minimal() + scale_fill_brewer(palette = "Accent", direction = 1) +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Accent", direction = 1) +
   theme(
     plot.title = element_text(family = "Verdana", size = 14),
-    legend.position = "none",
     panel.grid.minor.x = element_blank(),
     panel.grid.major.x = element_blank()
   )
